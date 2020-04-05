1f36176318a844726b3e9bb2da77e68a2aa5a7be
japerry911 <jack.e.perry@outlook.com>
2020-04-05 13:47:02 -0600

    Restyled WelcomeScreen

--- Frontend/src/screens/WelcomeScreen.js
+++ Frontend/src/screens/WelcomeScreen.js
@@ -1,20 +1,80 @@
 import React from 'react';
-import { StyleSheet } from 'react-native';
-import { Container, Text } from 'native-base';
+import { StyleSheet, ImageBackground, TouchableOpacity } from 'react-native';
+import { Text, View, Body, Footer } from 'native-base';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
+import Colors from '../constants/colors';
 
 const WelcomeScreen = ({ navigation }) => {
     return (
-        <Container style={styles.containerScreenStyle}>
-            <Text>WELCOME SCREEN</Text>
-        </Container>
+        <ImageBackground 
+            style={styles.backgroundImageStyle}
+            source={require('../../assets/Library.png')}
+            resizeMode='stretch'
+        >
+            <View style={styles.childComponents}>
+                <Body style={styles.bodyStyle}>
+                    <Text style={styles.titleStyle}>
+                        Skylord's{'\n\t\t\t'}Library
+                        <MaterialCommunityIcons
+                            name='paw'
+                            size={20}
+                            color={Colors.primary}
+                        />
+                    </Text>
+                    <Text style={styles.subtitleStyle}>
+                        Discover your favorite books with your favorite people<Text style={styles.highlightStyle}>.</Text>
+                    </Text>
+                </Body>
+                <TouchableOpacity>
+                <Footer style={styles.footerStyle}>
+                    <Text style={styles.footerTextStyle}>Get Started</Text>
+                </Footer>
+                </TouchableOpacity>
+            </View>
+        </ImageBackground>
     );
 };
 
 const styles = StyleSheet.create({
-    containerScreenStyle: {
-        justifyContent: 'center',
+    footerTextStyle: {
+        color: 'white',
+        fontSize: 30,
+        fontFamily: 'Avenir_bold',
+        marginTop: 30
+    },
+    footerStyle: {
+        backgroundColor: Colors.primary
+    },
+    childComponents: {
         flex: 1,
-        alignItems: 'center'
+        backgroundColor: 'rgba(0,0,0,0.5)'
+    },
+    pawImageStyle: {
+        height: 10,
+        width: 10,
+    },
+    highlightStyle: {
+        fontSize: 20,
+        fontFamily: 'Avenir_bold',
+        color: Colors.primary
+    },
+    subtitleStyle: {
+        color: 'white',
+        fontSize: 20,
+        fontFamily: 'Avenir_bold',
+        textAlign: 'center'
+    },
+    bodyStyle: {   
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    backgroundImageStyle: {
+        flex: 1
+    },
+    titleStyle: {
+        color: 'white',
+        fontFamily: 'Avenir_bold',
+        fontSize: 45
     }
 });
 
