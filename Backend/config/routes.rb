9e28b49e9480c1cf0153c19eb28c4b6cb06066b0
japerry911 <jack.e.perry@outlook.com>
2020-04-06 10:18:51 -0600

    Authentication working in Backend (Bcrypt) || sessions#login route

--- Backend/config/routes.rb
+++ Backend/config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   resources :books, only: [:index, :show]
   resources :users, only: [:index, :show, :create]
   resources :reviews, only: [:index, :show]
+  post '/login', to: 'sessions#login'
 end
