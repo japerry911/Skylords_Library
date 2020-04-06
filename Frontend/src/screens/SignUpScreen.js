49fe3018beff97967987b1ac7057b09f717b9d70
japerry911 <jack.e.perry@outlook.com>
2020-04-05 22:04:45 -0600

    Validation added

--- Frontend/src/screens/SignUpScreen.js
+++ Frontend/src/screens/SignUpScreen.js
@@ -1,9 +1,27 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { StyleSheet, View } from 'react-native';
-import { Container, Body, Text, Form, Label, Item, Input, Button } from 'native-base';
+import { Container, Body, Text, Form, Label, Item, Input, Button, Icon } from 'native-base';
 import Colors from '../constants/colors';
 
+const onFormSubmit = (username, age, password, callBack) => {};
+
 const SignUpScreen = () => {
+    const [username, setUsername] = useState('');
+    const [age, setAge] = useState('');
+    const [password, setPassword] = useState('');
+    const [confirmPassword, setConfirmPassword] = useState('');
+    const [passwordMatchingBool, setPasswordMatchingBool] = useState(false);
+    const [validationStatus, setValidationStatus] = useState(false);
+
+    useEffect(() => {
+        setPasswordMatchingBool(!(password !== confirmPassword || password === '' || confirmPassword === ''));
+        if (username !== '' && age !== '' && !(password !== confirmPassword || password === '' || confirmPassword === '')) {
+            setValidationStatus(true);
+        } else {
+            setValidationStatus(false);
+        }
+    }, [password, confirmPassword, username, age]);
+
     return (
         <Container style={styles.mainContainerStyle}>
             <Body>
@@ -24,32 +42,62 @@ const SignUpScreen = () => {
                             style={styles.formItemStyle}
                         >
                             <Label style={styles.roundedLabelStyle}>Enter your Username</Label>
-                            <Input />
+                            <Input
+                                value={username}
+                                onChangeText={newUsername => setUsername(newUsername)}
+                            />
                         </Item>
                         <Item 
                             floatingLabel
                             style={styles.formItemStyle}
                         >
                             <Label style={styles.roundedLabelStyle}>Enter your Age</Label>
-                            <Input />
+                            <Input
+                                value={age}
+                                onChangeText={newAge => setAge(newAge.replace(/[^0-9]/g, ''))}
+                                keyboardType='numeric'
+                                maxLength={3}
+                            />
                         </Item>
                         <Item 
                             floatingLabel
                             style={styles.formItemStyle}
+                            success={passwordMatchingBool}
+                            error={!passwordMatchingBool}
                         >
                             <Label style={styles.roundedLabelStyle}>Enter your Password</Label>
-                            <Input />
+                            <Input
+                                value={password}
+                                onChangeText={newPassword => setPassword(newPassword)}
+                                secureTextEntry
+                                autoCapitalize='none'
+                                autoCorrect={false}
+                            />
+                            <Icon name={passwordMatchingBool ? 'checkmark-circle' : 'close-circle'} />
                         </Item>
                         <Item 
                             floatingLabel                            
                             style={styles.formItemStyle}
+                            success={passwordMatchingBool}
+                            error={!passwordMatchingBool}
                         >
                             <Label style={styles.roundedLabelStyle}>Confirm your Password</Label>
-                            <Input />
+                            <Input 
+                                value={confirmPassword}
+                                onChangeText={newConfirmPassword => setConfirmPassword(newConfirmPassword)}
+                                secureTextEntry
+                                autoCapitalize='none'
+                                autoCorrect={false}
+                            />
+                            <Icon name={passwordMatchingBool ? 'checkmark-circle' : 'close-circle'} />
                         </Item>
                     </Form>
-                    <Button style={styles.signUpButtonStyle}>
+                    <Button 
+                        style={styles.signUpButtonStyle}
+                        disabled={!validationStatus}
+                    >
                         <Text style={styles.buttonTextStyle}>Sign Up</Text>
+                        <Icon name={validationStatus ? 'checkmark-circle' : 'close-circle'} />
                     </Button>
                     <Button style={styles.goBackButtonStyle}>
                         <Text style={styles.buttonTextStyle}>Go Back</Text>
