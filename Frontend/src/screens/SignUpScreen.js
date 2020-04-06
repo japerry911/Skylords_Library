c9eff67fa675c39a83456798bff64be1ca539f06
japerry911 <jack.e.perry@outlook.com>
2020-04-05 20:11:01 -0600

    General Layout done

--- Frontend/src/screens/SignUpScreen.js
+++ Frontend/src/screens/SignUpScreen.js
@@ -1,82 +1,113 @@
-import React, { useState } from 'react';
-import { StyleSheet } from 'react-native';
-import { Container, Header, Content, Form, Item, Label, Input, Button, Text } from 'native-base';
-import railsServer from '../api/railsServer';
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { Container, Body, Text, Form, Label, Item, Input, Button } from 'native-base';
+import Colors from '../constants/colors';
 
-const onFormSubmit = async (username, age, password, callback) => {
-    await railsServer.post('/users', { username, age, password });
-    callback();
-};
-
-const SignUpScreen = ({ navigation }) => {
-    const [username, setUsername] = useState('');
-    const [age, setAge] = useState('');
-    const [password, setPassword] = useState('');
-    const [confirmedPassword, setConfirmedPassword] = useState('');
-
+const SignUpScreen = () => {
     return (
-        <Container style={styles.screenStyle}>
-            <Header />
-            <Content>
-                <Form style={styles.formStyle}>
-                    <Item floatingLabel>
-                        <Label>Enter a Username</Label>
-                        <Input 
-                            value={username}
-                            onChangeText={newUsername => setUsername(newUsername)}
-                        /> 
-                    </Item>
-                    <Item floatingLabel>
-                        <Label>Enter your age</Label>
-                        <Input 
-                            keyboardType='numeric' 
-                            value={age}
-                            onChangeText={newAge => setAge(newAge)}
-                        />
-                    </Item>
-                    <Item floatingLabel>
-                        <Label>Enter a password</Label>
-                        <Input 
-                            secureTextEntry={true}
-                            value={password}
-                            onChangeText={newPassword => setPassword(newPassword)}
-                        />
-                    </Item>
-                    <Item floatingLabel>
-                        <Label>Confirm your password</Label>
-                        <Input
-                            secureTextEntry={true}
-                            value={confirmedPassword}
-                            onChangeText={newConfirmedPassword => setConfirmedPassword(newConfirmedPassword)}
-                        />
-                    </Item>
-                    <Button 
-                        style={styles.buttonStyle}
-                        onPress={() => navigation.navigate('Welcome')}
-                    >
-                        <Text>GO BACK</Text>
+        <Container style={styles.mainContainerStyle}>
+            <Body>
+                <Text 
+                    style={styles.mainHeaderStyle}
+                >
+                    Welcome to Skylord's Library
+                </Text>
+                <Text 
+                    style={styles.subtitleHeaderStyle}
+                >
+                    Please Sign Up to continue
+                </Text>
+                <View style={styles.formViewStyle}>
+                    <Form>
+                        <Item 
+                            floatingLabel
+                            style={styles.formItemStyle}
+                        >
+                            <Label style={styles.roundedLabelStyle}>Enter your Username</Label>
+                            <Input />
+                        </Item>
+                        <Item 
+                            floatingLabel
+                            style={styles.formItemStyle}
+                        >
+                            <Label style={styles.roundedLabelStyle}>Enter your Age</Label>
+                            <Input />
+                        </Item>
+                        <Item 
+                            floatingLabel
+                            style={styles.formItemStyle}
+                        >
+                            <Label style={styles.roundedLabelStyle}>Enter your Password</Label>
+                            <Input />
+                        </Item>
+                        <Item 
+                            floatingLabel                            
+                            style={styles.formItemStyle}
+                        >
+                            <Label style={styles.roundedLabelStyle}>Confirm your Password</Label>
+                            <Input />
+                        </Item>
+                    </Form>
+                    <Button style={styles.signUpButtonStyle}>
+                        <Text style={styles.buttonTextStyle}>Sign Up</Text>
                     </Button>
-                    <Button 
-                        style={styles.buttonStyle}
-                        onPress={() => onFormSubmit(username, age, password, () => navigation.navigate('Welcome'))}
-                    >
-                        <Text>SIGN UP {username}!</Text>
+                    <Button style={styles.goBackButtonStyle}>
+                        <Text style={styles.buttonTextStyle}>Go Back</Text>
                     </Button>
-                </Form>
-            </Content>
+                </View>
+            </Body>
         </Container>
     );
 };
 
 const styles = StyleSheet.create({
-    buttonStyle: {
-        margin: 10
+    formItemLabelStyle: {
+        color: Colors.accentLightGrayText,
+        fontFamily: 'Avenir_bold',
+        marginTop: 10
     },
-    screenStyle: {
-        flex: 1
+    roundedLabelStyle: {
+        color: Colors.accentLightGrayText
     },
-    formStyle: {
-        
+    buttonTextStyle: {
+        fontSize: 24,
+        fontFamily: 'Avenir_bold',
+        paddingTop: 5
+    },
+    formItemStyle: {
+        width: '80%'
+    },
+    signUpButtonStyle: {
+        backgroundColor: Colors.primaryOrange,
+        marginVertical: 15,
+        paddingHorizontal: 25
+    },
+    goBackButtonStyle: {
+        backgroundColor: Colors.accentLightOrange,
+        marginBottom: 25,
+        paddingHorizontal: 5
+    },
+    formViewStyle: {
+        borderRadius: 15,
+        minWidth: '90%',
+        backgroundColor: Colors.accentLightWhite,
+        marginTop: 30,
+        alignItems: 'center'
+    },
+    mainHeaderStyle: {
+        fontSize: 20,
+        fontFamily: 'Avenir_bold',
+        marginTop: 40,
+        color: Colors.accentLightGrayText   
+    },
+    subtitleHeaderStyle: {
+        marginTop: 10,
+        color: Colors.accentLightGrayText,
+        fontSize: 18,
+        fontFamily: 'Avenir_next'
+    },
+    mainContainerStyle: {
+        backgroundColor: Colors.accentLightGray
     }
 });
 
