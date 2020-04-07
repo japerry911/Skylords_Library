2f4ba27c8c423472ee0bb7d59a134815c917fce6
japerry911 <jack.e.perry@outlook.com>
2020-04-06 19:37:32 -0600

    Fixed memory leak in useEffect hook in HomeScreen

--- Frontend/src/screens/HomeScreen.js
+++ Frontend/src/screens/HomeScreen.js
@@ -2,21 +2,30 @@ import React, { useEffect, useState } from 'react';
 import { StyleSheet, View, TouchableOpacity, Image } from 'react-native';
 import { Container, Body, Footer, Text } from 'native-base';
 import Colors from '../constants/colors';
-import { SimpleLineIcons, FontAwesome, MaterialCommunityIcons, MaterialIcons } from '@expo/vector-icons';
+import { SimpleLineIcons, FontAwesome, MaterialCommunityIcons, MaterialIcons, FontAwesome5 } from '@expo/vector-icons';
 import railsServer from '../api/railsServer';
 import { Rating } from 'react-native-ratings';
+import FooterIconButton from '../components/FooterIconButton';
+import axios from 'axios';
 
 const HomeScreen = ({ navigation }) => {
     const user = navigation.getParam('user');
     const [mostRecentObject, setMostRecentObject] = useState({ user: {}, book: {}, review: {} });
-
+    
     useEffect(() => {
-        const fetchMostRecentReview = async () => {
-            const response = await railsServer.get('/most_recent_review');
-            setMostRecentObject(response.data);
-        }
+        const CancelToken = axios.CancelToken
+        const source = CancelToken.source()
 
-        fetchMostRecentReview();
+        try {
+            railsServer.get('/most_recent_review', { cancelToken: source.token })
+                .then(response => setMostRecentObject(response.data));
+        } catch (error) {
+            if (axios.isCancel(error)) {
+                console.log('Canceled');
+            } else {
+                throw error;
+            }
+        }
     }, []);
 
     return (
@@ -29,7 +38,7 @@ const HomeScreen = ({ navigation }) => {
                     Let's find your new favorite book.
                 </Text>
                 <View style={styles.bodyIconViewStyle}>
-                    <TouchableOpacity>
+                    <TouchableOpacity style={styles.iconTOStyle}>
                         <View style={styles.iconViewStyle}>
                             <SimpleLineIcons
                                 name='book-open'
@@ -41,7 +50,7 @@ const HomeScreen = ({ navigation }) => {
                             </Text>
                         </View>
                     </TouchableOpacity>
-                    <TouchableOpacity>
+                    <TouchableOpacity style={styles.iconTOStyle}>
                         <View style={styles.iconViewStyle}>
                             <FontAwesome
                                 name='pencil-square-o'
@@ -53,7 +62,7 @@ const HomeScreen = ({ navigation }) => {
                             </Text>
                         </View>
                     </TouchableOpacity>
-                    <TouchableOpacity>
+                    <TouchableOpacity style={styles.iconTOStyle}>
                         <View style={styles.iconViewStyle}>
                             <MaterialCommunityIcons
                                 name='fire'
@@ -65,7 +74,7 @@ const HomeScreen = ({ navigation }) => {
                             </Text>
                         </View>
                     </TouchableOpacity>
-                    <TouchableOpacity>
+                    <TouchableOpacity style={styles.iconTOStyle}>
                         <View style={styles.iconViewStyle}>
                             <MaterialIcons 
                                 name='message'
@@ -86,18 +95,12 @@ const HomeScreen = ({ navigation }) => {
                         <Image
                             source={{ uri: mostRecentObject.book.image_url }}
                             style={styles.mostRecentImageStyle}
-                        />                        
-                        <TouchableOpacity style={styles.bookmarkIconStyle}>
-                        <MaterialIcons
-                            name='bookmark'
-                            style={styles.bookmarkIconStyle}
-                            size={25}
-                        />
-                        </TouchableOpacity>
+                        />                       
                     </View>
                     <View style={styles.mostRecentReviewStyle}>
-                        <Text style={styles.mostRecentReviewTextStyle}>
-                            "{mostRecentObject.review.description}" {'\n\t'}- {mostRecentObject.user.username}
+                        <Text style={styles.mostRecentReviewItalStyle}>
+                            "{mostRecentObject.review.description}" {'\n\t'}- <Text style={styles.mostRecentReviewMedStyle}>
+                                {mostRecentObject.user.username}</Text>
                         </Text>
                         <View style={{alignItems: 'center', justifyContent: 'center'}}>
                             <Rating 
@@ -114,17 +117,50 @@ const HomeScreen = ({ navigation }) => {
                 </View>
             </Body>
             <Footer style={styles.footerStyle}>
-
+                <FooterIconButton
+                    iconComponent={<MaterialCommunityIcons
+                                        name='home-outline'
+                                        size={35}
+                                        style={styles.footerIconStyle}
+                                    />}
+                    onPress={() => {}}
+                />
+                <FooterIconButton
+                    iconComponent={<FontAwesome
+                                        name='star-o'
+                                        size={35}
+                                        style={styles.footerIconStyle}
+                                    />}
+                    onPress={() => {}}
+                />
+                <FooterIconButton
+                    iconComponent={<FontAwesome
+                                        name='bookmark-o'
+                                        size={32}
+                                        style={styles.footerIconStyle}
+                                    />}
+                    onPress={() => {}}
+                />
+                <FooterIconButton
+                    iconComponent={<FontAwesome
+                                        name='user-o'
+                                        size={32}
+                                        style={styles.footerIconStyle}
+                                    />}
+                    onPress={() => {}}
+                />
             </Footer>
         </Container>
     );
 };
 
 const styles = StyleSheet.create({
-    bookmarkIconStyle: {
-        position: 'absolute',
-        top: 0,
-        right: 0,
+    footerStyle: {
+        justifyContent: 'space-evenly',
+        backgroundColor: Colors.accentLightGray
+    },
+    footerIconStyle: {
+        marginTop: 5,
         color: Colors.primaryOrange
     },
     iconTextStyle: {
@@ -139,8 +175,10 @@ const styles = StyleSheet.create({
         opacity: .75
     },
     mostRecentViewStyle: {
-        justifyContent: 'center',
-        alignItems: 'center'
+        height: '80%',
+        alignItems: 'center',
+        justifyContent: 'space-between',
+        paddingBottom: '10%'
     },
     mostRecentReviewStyle: {
         backgroundColor: Colors.accentLightGray,
@@ -148,14 +186,18 @@ const styles = StyleSheet.create({
         marginVertical: 5,
         borderRadius: 10
     },
-    mostRecentReviewTextStyle: {
+    mostRecentReviewItalStyle: {
         color: Colors.accentLightGrayText,
         fontFamily: 'Avenir_italicize'
+    },
+    mostRecentReviewMedStyle: {
+        color: Colors.accentLightGrayText,
+        fontFamily: 'Avenir_medium'
     },
     mostRecentTitleStyle: {
         fontSize: 20,
         fontFamily: 'Avenir_medium',
-        marginTop: '10%',
+        marginTop: '5%',
         color: Colors.accentLightGrayText
     },
     mostRecentImageViewStyle: {
@@ -163,7 +205,8 @@ const styles = StyleSheet.create({
         alignItems: 'center',
         maxWidth: '90%',
         overflow: 'hidden',
-        marginTop: 20,
+        marginTop: 7,
+        borderRadius: 10
     },
     iconViewStyle: {
         backgroundColor: Colors.accentLightGray,
@@ -171,7 +214,13 @@ const styles = StyleSheet.create({
         paddingVertical: 5,
         borderRadius: 10,
         alignItems: 'center',
-        marginHorizontal: 20
+        marginHorizontal: 20,
+        width: '50%'
+    },
+    iconTOStyle: {
+        width: '50%',
+        justifyContent: 'center',
+        alignItems: 'center'
     },
     iconStyle: {
         color: Colors.primaryOrange
@@ -204,9 +253,6 @@ const styles = StyleSheet.create({
         height: '100%',
         borderRadius: 10,
         marginTop: '5%',
-    },
-    footerStyle: {
-        backgroundColor: Colors.accentLightGray
     }
 });
 
