302ee810be189345498b105ed26d077dd1526f6f
japerry911 <jack.e.perry@outlook.com>
2020-04-08 11:00:24 -0600

    bug style fix on homescreen

--- Frontend/src/screens/HomeScreen.js
+++ Frontend/src/screens/HomeScreen.js
@@ -218,7 +218,7 @@ const styles = StyleSheet.create({
         width: '90%',
         height: '100%',
         borderRadius: 10,
-        marginTop: '5%',
+        marginVertical: '5%',
     }
 });
 
