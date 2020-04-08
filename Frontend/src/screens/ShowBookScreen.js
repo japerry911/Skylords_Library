d6c3caf27c9b5c5ebd3ff2bafe1edda94f3dfbc8
japerry911 <jack.e.perry@outlook.com>
2020-04-08 13:46:44 -0600

    Header and BackButton added

--- Frontend/src/screens/ShowBookScreen.js
+++ Frontend/src/screens/ShowBookScreen.js
@@ -1,15 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Container, Text } from 'native-base';
-import { StyleSheet } from 'react-native';
+import { StyleSheet, TouchableOpacity, View } from 'react-native';
+import { MaterialIcons } from '@expo/vector-icons';
+import Colors from '../constants/colors';
 
-const ShowBookScreen = ({ route }) => {
+const ShowBookScreen = ({ route, navigation }) => {
+    const [bookDetails, setBookDetails] = useState({});
+    const bookId = route.params.bookId;
+
+    useEffect(() => {}, []);
+
     return (
         <Container>
-            <Text>Show Book Screen - {route.params.bookId} </Text>
+            <View style={styles.headerViewStyle}>
+                <TouchableOpacity onPress={() => navigation.pop()}>
+                    <MaterialIcons 
+                        name='chevron-left'
+                        size={40}
+                        style={styles.backIconStyle}
+                    />
+                </TouchableOpacity>
+                <Text style={styles.headerTitleStyle}>
+                    BookTitle!!
+                </Text>
+            </View>
         </Container>
     );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+    backIconStyle: {
+        height: 50,
+        color: Colors.primaryOrange
+    },
+    mainContainerStyle: {
+        backgroundColor: Colors.accentLightGray
+    },
+    headerViewStyle: {
+        marginTop: '5%',
+        marginLeft: '5%',
+        flexDirection: 'row',
+        alignItems: 'center',
+        
+    },
+    headerTitleStyle: {
+        color: Colors.accentLightGrayText,
+        fontSize: 24,
+        fontFamily: 'Avenir_bold'
+    }
+});
 
 export default ShowBookScreen;
