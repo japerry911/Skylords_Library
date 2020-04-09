df9973d927bb16c5a37738e4b7f405a787a40bc5
japerry911 <jack.e.perry@outlook.com>
2020-04-09 16:02:43 -0600

    untested draft of userContext done

--- Frontend/src/contexts/userContext.js
+++ Frontend/src/contexts/userContext.js
@@ -1,22 +1,44 @@
 import { createContext } from 'react';
+import railsServer from '../api/railsServer';
+import createDataContext from './createDataContext';
 
-const userContext = createContext({user: {}});
+const INITIAL_STATE = {user: {}};
+
+const userContext = createContext(INITIAL_STATE);
 
 const userReducer = ((state, action) => {
     switch (action.type) {
-        case 'MODIFY_CURRENT_USER':
+        case 'MODIFY_USER':
             return (
                 { 
                     ...state,
                     user: action.payload
                 }
             );
+
+        case 'CLEAR_USER':
+            return INITIAL_STATE;
         
         default: 
             return state;
     }
 });
 
-export {
-    userContext
+const signInUser = dispatch => {
+    return async (username, password) => {
+        try {
+            const response = await railsServer.post('/login', { username, password });
+            dispatch({ type: 'MODIFY_USER', payload: response.user });
+            return true;
+        } catch (error) {
+            console.log(error);
+            return false;
+        }
+    };
 };
+
+const signOutUser = dispatch => {
+    return async () => dispatch({ type: 'CLEAR_USER' });
+};
+
+export const { Context, Provider } = createDataContext(userReducer, { signInUser, signOutUser }, INITIAL_STATE);
