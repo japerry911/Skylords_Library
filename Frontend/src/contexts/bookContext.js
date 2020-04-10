0764bbf35218e925c87f9a3aa4feb74524944911
japerry911 <jack.e.perry@outlook.com>
2020-04-10 15:42:52 -0600

    taking break, committing a few additions to bookContext

--- Frontend/src/contexts/bookContext.js
+++ Frontend/src/contexts/bookContext.js
@@ -10,6 +10,10 @@ const bookReducer = (state, action) => {
     }
 };
 
-const getBooks = dispatch => {};
+const getBooks = dispatch => {
+    return async () => {
+        const getBooksResponse = await railsServer.get('/books');
+    };
+};
 
 export const { Context, Provider } = createDataContext(bookReducer, { getBooks }, INITIAL_STATE);
