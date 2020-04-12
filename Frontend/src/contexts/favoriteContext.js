604576de0468e929e13bf8a3dd550c27f9c5e95e
japerry911 <jack.e.perry@outlook.com>
2020-04-12 13:15:52 -0600

    Added GET_FAVORITES action

--- Frontend/src/contexts/favoriteContext.js
+++ Frontend/src/contexts/favoriteContext.js
@@ -5,9 +5,19 @@ const INITIAL_STATE = { favorites: [] };
 
 const favoriteReducer = (state, action) => {
     switch (action.type) {
+        case 'GET_FAVORITES':
+            return { ...state, favorites: action.payload };
+
         default:
             return state;
     }
 };
 
-export const { Context, Provider } = createDataContext(favoriteReducer, {}, INITIAL_STATE);
+const getFavorites = dispatch => {
+    return async () => {
+        const getFavoritesResponse = await railsServer.get('/favorites');
+        dispatch({ type: 'GET_FAVORITES', payload: getFavoritesResponse.data.favorites });
+    };
+};
+
+export const { Context, Provider } = createDataContext(favoriteReducer, { getFavorites }, INITIAL_STATE);
