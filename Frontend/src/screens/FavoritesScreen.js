fe551f5daac2131f6630c6ec204ab5e363292792
japerry911 <jack.e.perry@outlook.com>
2020-04-12 13:20:27 -0600

    FavoritesScreen boilerplate setup

---
+++ Frontend/src/screens/FavoritesScreen.js
@@ -0,0 +1,14 @@
+import React, { useContext, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+
+const FavoritesScreen = () => {
+    return (
+        <View style={styles.mainViewStyle}>
+            <Text>Favorites Screen</Text>
+        </View>
+    );
+};
+
+const styles = StyleSheet.create({});
+
+export default FavoritesScreen;
