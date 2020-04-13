9a59186eb3bc38ce8db797f5072ec0e95fbc6075
japerry911 <jack.e.perry@outlook.com>
2020-04-12 18:50:11 -0600

    Added DELETE_FAVORITE action to favoriteContext

--- Frontend/src/contexts/favoriteContext.js
+++ Frontend/src/contexts/favoriteContext.js
@@ -8,6 +8,10 @@ const favoriteReducer = (state, action) => {
         case 'GET_FAVORITES':
             return { ...state, favorites: action.payload };
 
+        case 'DELETE_FAVORITE': 
+            const newFavorites = state.favorites.filter(favorite => favorite.id !== action.payload);
+            return { ...state, favorites: newFavorites };
+
         default:
             return state;
     }
@@ -17,6 +21,13 @@ const getFavorites = dispatch => {
     return async () => {
         const getFavoritesResponse = await railsServer.get('/favorites');
         dispatch({ type: 'GET_FAVORITES', payload: getFavoritesResponse.data.favorites });
+    };
+};
+
+const deleteFavorite = dispatch => {
+    return async id => {
+        await railsServer.delete(`/favorites/${id}`);
+        dispatch({ type: 'DELETE_FAVORITE', payload: id });
     };
 };
 
