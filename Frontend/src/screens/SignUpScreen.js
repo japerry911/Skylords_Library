e9b8c3eabd30a17c34abd8dcdb05b8fc8220e677
japerry911 <jack.e.perry@outlook.com>
2020-04-14 08:57:40 -0600

    updated form to include email and phone and updated validation

--- Frontend/src/screens/SignUpScreen.js
+++ Frontend/src/screens/SignUpScreen.js
@@ -7,10 +7,13 @@ import { Context as UserContext } from '../contexts/userContext';
 const SignUpScreen = ({ navigation }) => {
     const [username, setUsername] = useState('');
     const [age, setAge] = useState('');
+    const [email, setEmail] = useState('');
+    const [phone, setPhone] = useState('');
     const [password, setPassword] = useState('');
     const [confirmPassword, setConfirmPassword] = useState('');
     const [passwordMatchingBool, setPasswordMatchingBool] = useState(false);
     const [validationStatus, setValidationStatus] = useState(false);
+    const emailRegex = new RegExp(/(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])/);
 
     const userContext = useContext(UserContext);
     const { signUpUser } = userContext;
@@ -26,12 +29,13 @@ const SignUpScreen = ({ navigation }) => {
 
     useEffect(() => {
         setPasswordMatchingBool(!(password !== confirmPassword || password === '' || confirmPassword === ''));
-        if (username !== '' && age !== '' && !(password !== confirmPassword || password === '' || confirmPassword === '')) {
+        if (username !== '' && age !== '' && emailRegex.test(email) && phone.length === 10 &&
+            !(password !== confirmPassword || password === '' || confirmPassword === '')) {
             setValidationStatus(true);
         } else {
             setValidationStatus(false);
         }
-    }, [password, confirmPassword, username, age]);
+    }, [password, confirmPassword, username, age, email, phone]);
 
     return (
         <View style={styles.mainViewStyle}>
@@ -50,16 +54,21 @@ const SignUpScreen = ({ navigation }) => {
                     <Item 
                         floatingLabel
                         style={styles.formItemStyle}
+                        success={username !== ''}
+                        error={username === ''}
                     >
                         <Label style={styles.formItemLabelStyle}>Enter your Username</Label>
                         <Input
                             value={username}
                             onChangeText={newUsername => setUsername(newUsername)}
                         />
+                        <Icon name={username !== '' ? 'checkmark-circle' : 'close-circle'} />
                     </Item>
                     <Item 
                         floatingLabel
                         style={styles.formItemStyle}
+                        success={age !== ''}
+                        error={age === ''}
                     >
                         <Label style={styles.formItemLabelStyle}>Enter your Age</Label>
                         <Input
@@ -68,6 +77,35 @@ const SignUpScreen = ({ navigation }) => {
                             keyboardType='numeric'
                             maxLength={3}
                         />
+                        <Icon name={age !== '' ? 'checkmark-circle' : 'close-circle'} />
+                    </Item>
+                    <Item 
+                        floatingLabel
+                        style={styles.formItemStyle}
+                        success={emailRegex.test(email)}
+                        error={!emailRegex.test(email)}
+                    >
+                        <Label style={styles.formItemLabelStyle}>Enter your Email</Label>
+                        <Input
+                            value={email}
+                            onChangeText={newEmail => setEmail(newEmail)}
+                        />
+                        <Icon name={emailRegex.test(email) ? 'checkmark-circle' : 'close-circle'} />
+                    </Item>
+                    <Item 
+                        floatingLabel
+                        style={styles.formItemStyle}
+                        success={phone.length === 10}
+                        error={phone.length < 10}
+                    >
+                        <Label style={styles.formItemLabelStyle}>Enter your Phone Number</Label>
+                        <Input
+                            value={phone}
+                            onChangeText={newPhone => setPhone(newPhone.replace(/[^0-9]/g, ''))}
+                            keyboardType='numeric'
+                            maxLength={10}
+                        />
+                         <Icon name={phone.length === 10 ? 'checkmark-circle' : 'close-circle'} />
                     </Item>
                     <Item 
                         floatingLabel
